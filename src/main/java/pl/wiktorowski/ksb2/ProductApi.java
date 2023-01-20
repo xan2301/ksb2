d4ba52a225e59758af3dc69c38616fdc69f87447
xan2301 <2silverghost2@gmail.com>
2023-01-20 17:46:39 +0100

    v2

--- src/main/java/pl/wiktorowski/ksb2/ProductApi.java
+++ src/main/java/pl/wiktorowski/ksb2/ProductApi.java
@@ -1,19 +1,42 @@
 package pl.wiktorowski.ksb2;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("products")
 
 public class ProductApi {
 
 
-    @GetMapping("/products")
-    public String getProducts () {
+    @GetMapping
+    public String getProducts ( @RequestParam String name , @RequestParam String surname) {
 
-        return "Hello World with Get";
+        return "Hello " + name + " " + surname;
     }
+
+
+
+
+
+   @PostMapping
+    public String postProducts () {
+
+        return "Hello World with Post";
+    }
+
+    @PutMapping
+    public String putProducts () {
+
+        return "Hello World with Put";
+    }
+
+    @DeleteMapping
+    public String removeProducts () {
+
+        return "Hello World with Delete";
+    }
+
 
 
 }
