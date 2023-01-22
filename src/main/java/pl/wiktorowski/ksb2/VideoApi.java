ff2e0f7ffb3108ac42a21a8d5e643bdcb6ccf6a2
xan2301 <2silverghost2@gmail.com>
2023-01-22 19:32:02 +0100

    19 Rest v1

--- src/main/java/pl/wiktorowski/ksb2/VideoApi.java
+++ src/main/java/pl/wiktorowski/ksb2/VideoApi.java
@@ -3,10 +3,7 @@ package pl.wiktorowski.ksb2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,21 +18,21 @@ public class VideoApi {
 
     public VideoApi() {
         this.videoList = new ArrayList<>();
-        videoList.add( new Video(1L,"Wyjaśniam Gdzie Przepadłem i Przedstawiam Propozycje Wspólnego Zaangażowania Się w WOŚP [Daily Vlog]", "https://youtu.be/1ZAF9WQuv6Y"));
-        videoList.add( new Video(2L,"Spring Boot We Współpracy Z JDBC", "https://youtu.be/2xmxYBJdOfA"));
+        videoList.add(new Video(1L, "Wyjaśniam Gdzie Przepadłem i Przedstawiam Propozycje Wspólnego Zaangażowania Się w WOŚP [Daily Vlog]", "https://youtu.be/1ZAF9WQuv6Y"));
+        videoList.add(new Video(2L, "Spring Boot We Współpracy Z JDBC", "https://youtu.be/2xmxYBJdOfA"));
     }
 
     @GetMapping
-    public ResponseEntity<List<Video>> getVideos(){
+    public ResponseEntity<List<Video>> getVideos() {
 
         return new ResponseEntity<>(videoList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Video> getVideoByID(@PathVariable long id){
+    public ResponseEntity<Video> getVideoByID(@PathVariable long id) {
 
         Optional<Video> first = videoList.stream().filter(video -> video.getId() == id).findFirst();
-        if(first.isPresent()) {
+        if (first.isPresent()) {
 
 
             return new ResponseEntity<>(first.get(), HttpStatus.OK);
@@ -44,7 +41,12 @@ public class VideoApi {
 
     }
 
+    @PostMapping
 
+    public String addVideo(@RequestBody Video video) {
+            return "";
+
+    }
 
 
 
